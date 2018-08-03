60480c801f8d3887e06909bf84b74b66c5196f87
Radi Cho <radicho123@gmail.com>
2018-08-03 23:55:27 +0300

    Initialize firebase functions.

--- app/src/main/java/com/rsg/SciSearch/MainActivity.kt
+++ app/src/main/java/com/rsg/SciSearch/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.rsg.SciSearch
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,5 +45,37 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ArticleActivity::class.java)
             startActivityForResult(intent, 1)
         })
+
+        val Text = TextView(this)
+        mainContainer.addView(Text)
+        getData(Text)
+    }
+
+    private fun getData(element: TextView) {
+        val url = "https://us-central1-sci-search.cloudfunctions.net/getData"
+        val obj = URL(url)
+        Thread(Runnable {
+            try {
+                with(obj.openConnection() as HttpURLConnection) {
+                    // optional default is GET
+                    requestMethod = "GET"
+
+                    BufferedReader(InputStreamReader(inputStream)).use {
+                        val response = StringBuffer()
+
+                        var inputLine = it.readLine()
+                        while (inputLine != null) {
+                            response.append(inputLine)
+                            inputLine = it.readLine()
+                        }
+
+                        element.post({ element.setText(response.toString()) })
+                    }
+                }
+            } catch (e: IOException) {
+                // TODO: Add Firebase/Fabric Crashlytics solution here
+                element.post({ element.setText("Error") })
+            }
+        }).start()
     }
 }
---
+++ firebase/functions/lib/index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+exports.getData = functions.https.onRequest((req, res) => {
+    var articles = db.collection("articles");
+    var query = articles
+        .where("test", "==", true)
+        .limit(1)
+        .get()
+        .then(snapshot => {
+        snapshot.forEach(doc => {
+            res.status(200).send(JSON.stringify(doc.data()));
+            console.log(doc.id, "=>", doc.data());
+        });
+    })
+        .catch(err => {
+        console.log("Error getting documents", err);
+    });
+});
+//# sourceMappingURL=index.js.map
